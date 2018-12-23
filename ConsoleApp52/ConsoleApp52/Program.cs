feb31aed9aeb915555bf77e94213e0ec10ee4af4
justbegginer <46086383+justbegginer@users.noreply.github.com>
2018-12-23 16:04:19 +0300

    первая версия

---
+++ ConsoleApp52/ConsoleApp52/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[10];
+            massive(mass);
+
+        }
+        public static void massive(int[] mass )
+        {
+            Random random = new Random();
+            for(int i=0;i<mass.Length;i++)
+            {
+               int x = random.Next(1, 20);
+                mass[i] = x;
+            }
+            realization(mass);
+        }
+        public static void realization(int[] mass)
+        {           
+            int x = 0;
+            usersfalse(x, out x,mass);           
+            int y = 0;
+            usersfalse(y, out y,mass);
+            int z = mass[x] + mass[y];
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+        public static void usersfalse(int x,out int z,int[] mass)
+        {
+            try
+            {
+                Console.WriteLine("Введите номер массива");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Номер массива соответствует числу  " + mass[x]);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("error 404");
+                string[] return0 = new string[0];
+                Main(return0);
+            }
+            z = x;
+        }
+        public static void usersfalsemass(int[] mass,int u)
+        {
+            //try
+            //{
+            //    Console.WriteLine("Номер массива соответствует числу  " + mass[u]);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("error 403");
+            //    string[] return0 = new string[0];
+            //    Main(return0);
+            //}
+        }
+        
+
+    }
+
+}
